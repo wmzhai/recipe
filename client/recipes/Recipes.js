08cbf2fea1b4c5cae25b548a00b4d3c774517660
Weiming Zhai <wmzhai@gmail.com>
2016-01-26 20:32:54 +0800

    列表显示

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,1 +1,12 @@
 Meteor.subscribe('recipes');
+
+Template.Recipes.helpers({
+  recipes: function() {
+    options = {
+      sort: {
+        createdAt: -1
+      }
+    };
+    return Recipes.find({},options);
+  }
+});
