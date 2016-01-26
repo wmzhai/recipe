2c23d25919923f31859d2f5c4bf16e0e837f53ff
Weiming Zhai <wmzhai@gmail.com>
2016-01-26 21:35:07 +0800

    取消延时

--- server/publish.js
+++ server/publish.js
@@ -1,6 +1,5 @@
-Meteor.publish('recipes', function() {
+Meteor.publish('recipes', function () {
+  //Meteor._sleepForMs(5000);
 
-  Meteor._sleepForMs( 5000);
-
-  return Recipes.find({owner:this.userId});
+  return Recipes.find({owner: this.userId});
 });
