155d7006bcfe530920b0732915f7ce00342a333e
Weiming Zhai <wmzhai@gmail.com>
2016-01-28 20:27:43 +0800

    删除数据

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,6 +1,9 @@
 Template.Recipe.events({
   'click .toggle-menu': function() {
     Meteor.call('toggleMenuItem', this._id, this.inMenu);
+  },
+  'click .fa-trash': function(){
+    Meteor.call('deleteRecipe',this._id);
   }
 });
 
